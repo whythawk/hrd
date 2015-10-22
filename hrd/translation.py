cb6e0a63e21fd2319ace7de491ddc363e40aba6d
Toby Dacre <toby.junk@gmail.com>
2015-10-22 09:34:47 +0100

    fix for translation sequence

--- hrd/translation.py
+++ hrd/translation.py
@@ -47,15 +47,6 @@ def get_translations(quiet=True):
 
     catalog = Catalog(project='hrd')
 
-    sql = """
-        SELECT max(id) FROM translation;
-    """
-
-    result = conn.execute(sql).first()
-
-    max_id = result[0]
-    if not max_id:
-        max_id = 0
 
     for filename, lineno, message, comments, context in extracted:
         filepath = os.path.normpath(os.path.join(DIR, filename))
@@ -79,11 +70,10 @@ def get_translations(quiet=True):
         result = conn.execute(sql, values).first()
         if result is None:
             sql = """
-                INSERT INTO translation (id, string, plural, lang, active)
-                VALUES (%s, %s, %s, 'en', '1');
+                INSERT INTO translation (string, plural, lang, active)
+                VALUES (%s, %s, 'en', '1');
             """
-            max_id += 1
-            conn.execute(sql, (max_id,) + values)
+            conn.execute(sql, values)
         elif result[0] == 0:
             sql = """
                 UPDATE translation
@@ -138,15 +128,6 @@ def create_fake_trans(lang):
     sql = "DELETE FROM translation WHERE lang = %s"
     result = conn.execute(sql, lang)
 
-    sql = """
-        SELECT max(id) FROM translation;
-    """
-
-    result = conn.execute(sql).first()
-
-    max_id = result[0]
-    if not max_id:
-        max_id = 0
 
     sql = """
         SELECT DISTINCT string, plural FROM translation
@@ -156,14 +137,13 @@ def create_fake_trans(lang):
     result = conn.execute(sql)
     values = []
     for row in result:
-        max_id += 1
         values.append(
-            (max_id, row.string, row.plural, lang, mangle(row.string), mangle(row.plural))
+            (row.string, row.plural, lang, mangle(row.string), mangle(row.plural))
         )
 
     sql = """
-        INSERT INTO translation (id, string, plural, lang, active, trans0, trans1)
-        VALUES (%s, %s, %s, %s, '1', %s, %s);
+        INSERT INTO translation (string, plural, lang, active, trans0, trans1)
+        VALUES (%s, %s, %s, '1', %s, %s);
     """
     for value in values:
         conn.execute(sql, value)
