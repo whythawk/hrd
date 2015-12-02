5b52eac4950a41f94ab787bac61fe4ab74984d40
Toby Dacre <toby.junk@gmail.com>
2015-12-02 12:17:48 +0000

    prevent db migrate

--- hrd/migrate.py
+++ hrd/migrate.py
@@ -1,111 +1,111 @@
 from hrd import db
 
 
-sql = '''
-ALTER TABLE organisation
-    ADD private BOOLEAN;
-'''
-
-try:
-    db.engine.execute(sql)
-except:
-    pass
-
-
-sql = '''
-ALTER TABLE cms
-    ADD private BOOLEAN;
-'''
-
-try:
-    db.engine.execute(sql)
-except:
-    pass
-
-
-sql = '''
-CREATE TABLE translation (
-    id INTEGER NOT NULL,
-    string VARCHAR(250),
-    plural VARCHAR(250),
-    lang VARCHAR(2) NOT NULL,
-    active BOOLEAN,
-    trans0 VARCHAR(250),
-    trans1 VARCHAR(250),
-    trans2 VARCHAR(250),
-    trans3 VARCHAR(250),
-    trans4 VARCHAR(250),
-    trans5 VARCHAR(250),
-    PRIMARY KEY (id)
-);
-'''
-
-try:
-    db.engine.execute(sql)
-except:
-    pass
-
-
-sql = '''
-ALTER TABLE users
-    ADD ga_enabled BOOLEAN NOT NULL DEFAULT True,
-    ADD ga_key VARCHAR(50),
-    ADD reset_code VARCHAR(50),
-    ADD reset_date TIMESTAMP;
-'''
-
-try:
-    db.engine.execute(sql)
-except:
-    pass
-
-sql = '''
-ALTER TABLE users
-    ADD organization VARCHAR(50);
-'''
-
-try:
-    db.engine.execute(sql)
-except:
-    pass
-
-sql = '''
-ALTER TABLE users
-    ADD realname VARCHAR(250),
-    ADD position VARCHAR(250);
-'''
-
-try:
-    db.engine.execute(sql)
-except:
-    pass
-
-sql = '''
-ALTER TABLE users
-    ADD forum_digest BOOLEAN DEFAULT true
-'''
-
-try:
-    db.engine.execute(sql)
-except:
-    pass
-
-sql = '''
-ALTER TABLE users
-    ADD prefered_lang VARCHAR(10)
-'''
-
-try:
-    db.engine.execute(sql)
-except:
-    pass
-
-sql = '''
-ALTER TABLE category
-    ADD consolidate BOOLEAN DEFAULT false
-'''
-
-try:
-    db.engine.execute(sql)
-except:
-    pass
+##sql = '''
+##ALTER TABLE organisation
+##    ADD private BOOLEAN;
+##'''
+##
+##try:
+##    db.engine.execute(sql)
+##except:
+##    pass
+##
+##
+##sql = '''
+##ALTER TABLE cms
+##    ADD private BOOLEAN;
+##'''
+##
+##try:
+##    db.engine.execute(sql)
+##except:
+##    pass
+##
+##
+##sql = '''
+##CREATE TABLE translation (
+##    id INTEGER NOT NULL,
+##    string VARCHAR(250),
+##    plural VARCHAR(250),
+##    lang VARCHAR(2) NOT NULL,
+##    active BOOLEAN,
+##    trans0 VARCHAR(250),
+##    trans1 VARCHAR(250),
+##    trans2 VARCHAR(250),
+##    trans3 VARCHAR(250),
+##    trans4 VARCHAR(250),
+##    trans5 VARCHAR(250),
+##    PRIMARY KEY (id)
+##);
+##'''
+##
+##try:
+##    db.engine.execute(sql)
+##except:
+##    pass
+##
+##
+##sql = '''
+##ALTER TABLE users
+##    ADD ga_enabled BOOLEAN NOT NULL DEFAULT True,
+##    ADD ga_key VARCHAR(50),
+##    ADD reset_code VARCHAR(50),
+##    ADD reset_date TIMESTAMP;
+##'''
+##
+##try:
+##    db.engine.execute(sql)
+##except:
+##    pass
+##
+##sql = '''
+##ALTER TABLE users
+##    ADD organization VARCHAR(50);
+##'''
+##
+##try:
+##    db.engine.execute(sql)
+##except:
+##    pass
+##
+##sql = '''
+##ALTER TABLE users
+##    ADD realname VARCHAR(250),
+##    ADD position VARCHAR(250);
+##'''
+##
+##try:
+##    db.engine.execute(sql)
+##except:
+##    pass
+##
+##sql = '''
+##ALTER TABLE users
+##    ADD forum_digest BOOLEAN DEFAULT true
+##'''
+##
+##try:
+##    db.engine.execute(sql)
+##except:
+##    pass
+##
+##sql = '''
+##ALTER TABLE users
+##    ADD prefered_lang VARCHAR(10)
+##'''
+##
+##try:
+##    db.engine.execute(sql)
+##except:
+##    pass
+##
+##sql = '''
+##ALTER TABLE category
+##    ADD consolidate BOOLEAN DEFAULT false
+##'''
+##
+##try:
+##    db.engine.execute(sql)
+##except:
+##    pass
